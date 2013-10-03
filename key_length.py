9fcf74df4d0b07511715fc608935139ad6dfcc39
Victoria Bobey <vbobey@ualberta.ca>
2013-10-03 16:33:40 -0600

    ;)

--- key_length.py
+++ key_length.py
@@ -1,10 +1,12 @@
 # Functions to compute potential key length by finding patterns in the ciphertext.
-file = open('ciphertext1', 'rb')
+file = open('ciphertext2', 'rb')
 
 # Read the first byte in the file, which is position 0
 file_position = 0
 byte = file.read(1)
-check_byte = {byte} 
+
+# For filtering results of distance totals so low totals are not displayed
+FILTER_THRESHHOLD = 900
 
 # Dictionary to keep track of byte distances 
 all_dist = dict()
@@ -14,34 +16,35 @@ while True: # Using this because byte != EOF doesnt work in python
     if (len(byte) == 0):
         break
     
-    # Check byte has not been looked at already.
-    if (byte not in check_byte):
-        file.seek(file_position+1)
+    file.seek(file_position+1)
+    cur_byte = file.read(1)
+    while True:
+        # Check we are not at EOF
+        if (len(cur_byte) == 0):
+            break
+        # Assume key is not larger than 200
+        elif ((file.tell() - file_position) > 200):
+            break
+        else: pass
+        
+        # Check if cur_byte matches byte
+        if (cur_byte == byte):
+            # Calculate distance between positions in file
+            distance = (file.tell() - file_position) -1
+            # Check if distance is in dictionary, if so increment value
+            if (distance in all_dist):
+                all_dist[distance] += 1
+            else:
+                all_dist[distance] = 1
+        else: pass
+        # Move to next byte
         cur_byte = file.read(1)
-        while True:
-            # Check we are not at EOF
-            if (len(cur_byte) == 0):
-                break 
-            # Check if cur_byte matches byte
-            if (cur_byte == byte):
-                # Calculate distance between positions in file
-                distance = (file.tell() - file_position) -1
-                # Check if distance is in dictionary, if so increment value
-                if (distance in all_dist):
-                    all_dist[distance] += 1
-                else:
-                    all_dist[distance] = 1
-            else: pass
-            # Move to next byte
-            cur_byte = file.read(1)
-        byte = file.read(1)
-    # If byte has already been checked, move on
-    else: pass
+
     # Increment file position, move to that position, read next byte
     file_position = file_position + 1
     file.seek(file_position)
     byte = file.read(1)
-
+    
 print (all_dist)
 
 """
@@ -49,8 +52,6 @@ Filter results and display anything greater than FILTER_THRESHHOLD
 # Results are displayed in an (x,y) format. x is the distance and y is
 the number of times this distance occurs throughout the file.
 """
-FILTER_THRESHHOLD = 10
-
 Frequent_dists = [(x,y) for x, y in all_dist.items() if y > FILTER_THRESHHOLD]
 
 print ("\nFILTERED RESULTS: (x,y) x is dist between keys & y is frequency")
--- readme.txt
+++ readme.txt
@@ -1,5 +1,19 @@
 CMPUT 333 Assignment 1
 Group 4: Victoria Bobey, Sarah Morris, Troy Pavlek
 
-Question 3 encryption key: group4
+Running our files on lab machines:
+python key_length.py
+
+Question 1:
+To find the length of the key, used the file key_length.py. Looking at the distances 
+with the largest totals associated with them, we determined all the distances were multipules
+of 8.
+
+Question 2:
+Used the same file as question 1, but because ciphertext1 was substanially longer, we had to
+assume the key was smaller than 200 because the run time was too long. Luckily this showed the key length 
+was 33, with all other distances with large totals multiples of 33.
+
+Question 3:
+encryption key: group4
 
